7c3191d461a027ed4261663e8077bb0dc84f73ce
Mohammad Taufiqurrohman <ufiq@outlook.co.id>
2020-04-28 14:38:03 +0700

    Perbaikan Saldo Awal

--- app/Http/Controllers/Sekolah/SaldoAwalController.php
+++ app/Http/Controllers/Sekolah/SaldoAwalController.php
@@ -50,37 +50,37 @@ class SaldoAwalController extends Controller
     public function hitung(Request $request, $id)
     {
     	$sekolah = Auth::user();
-    	$sa= $sekolah->saldo_awals()->findOrFail($id);
-    	$ta= $request->cookie('ta');
-    	// return json_encode($sa);
+        $sa= $sekolah->saldo_awals()->findOrFail($id);
+        $ta= $request->cookie('ta');
+        // return json_encode($sa);
 
-    	$bulan = $sa->periode->format('n');
-    	$fromDate = Carbon::createFromDate($ta, $bulan-1, 1)->format('Y-m-d');
+        $bulan = $sa->periode->format('n');
+        $fromDate = Carbon::createFromDate($ta, $bulan-1, 1)->format('Y-m-d');
         $tillDate = Carbon::createFromDate($ta, $bulan-1, 1)->endOfMonth()->format('Y-m-d');
 
         if (($bulan-1)==1) {
-        	$saldo_awal = $sekolah->pendapatans()
-        	->where('sumber', 'SILPA BOS')
-        	->whereBetween('tanggal',[$fromDate, $tillDate])->get();
-        	$saldoawal_bank = $saldo_awal->sum('nominal');
-        	$saldoawal_tunai = 0;
+            $saldo_awal = $sekolah->pendapatans()
+            ->where('sumber', 'SILPA BOS')
+            ->whereBetween('tanggal',[$fromDate, $tillDate])->get();
+            $saldoawal_bank = $saldo_awal->sum('nominal');
+            $saldoawal_tunai = 0;
 
-        	$pendapatan_sebulan = $sekolah->pendapatans()
-        	->with('transaksi')
-        	->where('sumber','!=', 'SILPA BOS')
-        	->whereBetween('tanggal',[$fromDate, $tillDate])->get();
+            $pendapatan_sebulan = $sekolah->pendapatans()
+            ->with('transaksi')
+            ->where('sumber','!=', 'SILPA BOS')
+            ->whereBetween('tanggal',[$fromDate, $tillDate])->get();
             // return $pendapatan_sebulan;
         }
 
         else{
-        	$saldo_awal = $sekolah->saldo_awals()
-        	->where('periode', $fromDate)->get();
-        	$saldoawal_bank = $saldo_awal->sum('saldo_bank');
-        	$saldoawal_tunai = $saldo_awal->sum('saldo_tunai');
+            $saldo_awal = $sekolah->saldo_awals()
+            ->where('periode', $fromDate)->get();
+            $saldoawal_bank = $saldo_awal->sum('saldo_bank');
+            $saldoawal_tunai = $saldo_awal->sum('saldo_tunai');
 
-        	$pendapatan_sebulan = $sekolah->pendapatans()
-        	->with('transaksi')
-        	->whereBetween('tanggal',[$fromDate, $tillDate])->get();
+            $pendapatan_sebulan = $sekolah->pendapatans()
+            ->with('transaksi')
+            ->whereBetween('tanggal',[$fromDate, $tillDate])->get();
         }
 
         $belanja_sebulan = Belanja::with('transaksi')->npsn($sekolah->npsn)->whereBetween('tanggal',[$fromDate, $tillDate])->get();
@@ -92,9 +92,10 @@ class SaldoAwalController extends Controller
         ->where('transaksi.kas','T')->sum('nominal');
 
         $belanja_bank = $belanja_sebulan
-        ->where('transaksi.kas','B')->sum('nominal');
+        ->where('transaksi.kas','B')->sum('nilai');
         $belanja_tunai = $belanja_sebulan
-        ->where('transaksi.kas','T')->sum('nominal');
+        ->where('transaksi.kas','T')->sum('nilai');
+        // return $belanja_tunai;
 
         $transaksi_sebulan = $sekolah->kas_trx_details()
         ->with('kas_trx')
@@ -105,18 +106,17 @@ class SaldoAwalController extends Controller
         $tarik= $transaksi_sebulan->where('tipe','Pindah Buku')->sum('nominal');
 
 
-        $saldoakhir_bank= ($saldoawal_bank + $pendapatan_bank) - $belanja_bank;
-        $saldoakhir_tunai= ($saldoawal_tunai + $pendapatan_tunai) - $belanja_tunai;
-    	$saldoakhir = $saldoakhir_bank + $saldoakhir_tunai;
-
-    	try {
-    		$sa->saldo_bank = $saldoakhir_bank;
-    		$sa->saldo_tunai = $saldoakhir_tunai;
-    		$sa->save();
-    		return redirect()->route('sekolah.saldoawal.index')->with(['success' => 'Berhasil Update Saldo Awal']);
-    		
-    	} catch (\Exception $e) {
-    		return redirect()->route('sekolah.saldoawal.index')->withErrors('Error :'. $e->getMessage());
-    	}
+        $saldoakhir_bank= ($saldoawal_bank + $pendapatan_bank) - $belanja_bank - $tarik + $setor;
+        $saldoakhir_tunai= ($saldoawal_tunai + $pendapatan_tunai) - $belanja_tunai - $setor + $tarik;
+        // return $tarik;
+        try {
+            $sa->saldo_bank = $saldoakhir_bank;
+            $sa->saldo_tunai = $saldoakhir_tunai;
+            $sa->save();
+            return redirect()->route('sekolah.saldoawal.index')->with(['success' => 'Berhasil Update Saldo Awal']);
+            
+        } catch (\Exception $e) {
+            return redirect()->route('sekolah.saldoawal.index')->withErrors('Error :'. $e->getMessage());
+        }
     }
 }
